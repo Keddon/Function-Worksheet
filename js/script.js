3eefef750e41546d844757cd6419c247d75fa392
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 18:34:16 -0500

    Worksheet
    Created my first function which calculates the circumference based on
    the radius given.

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@
 Function Worksheet
  */
 
+/*
+Calculate the circumference of a circle.
+ */
 
+var circleAnswer = circleCalc(radius);
+
+function circleCalc(radius){
+    var result = radius * (2 * 3.14159);
+    return result;
+}
+
+circleCalc(6);
+console.log(circleAnswer);
