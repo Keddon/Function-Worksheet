60ead22e2611a08913da24d4f6ab7b72073b50b2
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 19:00:59 -0500

    Worksheet
    Added comments to the code

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ Calculate the circumference of a circle.
  */
 
 
-var circleAnswer = circleCalc(2);
+var circleAnswer = circleCalc(2); //stores the result of the function 'cirleCalc' and its argument
 
-function circleCalc(radius){
-    var circleresult = radius * (2 * 3.14159);
-    return circleresult;
+function circleCalc(radius){      // holds the parameter for the function
+    var circleresult = radius * (2 * 3.14159); //uses the argument passed in to calculate the radius
+    return circleresult;    // returns the value of the calculation to the variable 'circleAnswer'
 
 }
 
-console.log("The circumference is " + circleAnswer);
+console.log("The circumference is " + circleAnswer); //prints the results to console
 
 
